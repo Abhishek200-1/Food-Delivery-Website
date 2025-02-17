8942ee4857874990e8bbb1a2d9c30b40f2800292
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-02-17 23:15:39 +0530

    some css files updated

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,8 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-      <h1>Explore our menu</h1>
+      <p className='menu-text'>Menu</p>
+      <h1>Explore Our Best Menu</h1>
       <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index) => {
