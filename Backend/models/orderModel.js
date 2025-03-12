7bac822578ef3abd078419b072c7f610b5c701b5
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-12 20:33:56 +0530

    order model

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema({
     amount: { type: Number, required: true },
     address: { type: Object, required: true },
     status: { type: String, default: "Food Processing" },
-    date: { type: Date, default: Date.now }, // ✅ Corrected default date
+    date: { type: Date, default: Date.now }, 
     payment: { type: Boolean, default: false }
 });
 
