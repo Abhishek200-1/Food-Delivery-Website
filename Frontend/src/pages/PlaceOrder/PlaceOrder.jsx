b6bdc3114bdbda81c22204553a7ca79338ae73b1
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-12 19:52:45 +0530

    place order

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -95,17 +95,17 @@ const PlaceOrder = () => {
             <div>
               <div className="cart-total-details">
                 <p>Subtotal</p>
-                <p>${getTotalCartAmount()}</p>
+                <p>&#8377; {getTotalCartAmount()}</p>
               </div>
               <hr />
               <div className="cart-total-details">
                 <p>Delivery Fee</p>
-                <p>${getTotalCartAmount() === 0 ? 0 : 15}</p>
+                <p>&#8377; {getTotalCartAmount() === 0 ? 0 : 15}</p>
               </div>
               <hr />
               <div className="cart-total-details">
                 <b>Total</b>
-                <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 15}</b>
+                <b>&#8377; {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 15}</b>
               </div>
             </div>
             <button type='submit'>PROCEED TO CHECKOUT</button>
