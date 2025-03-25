b7afc4f408234c90f7e4aae0856b9acb279889e8
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-25 22:36:00 +0530

    updated add.js

--- Admin/src/pages/Add/Add.jsx
+++ Admin/src/pages/Add/Add.jsx
@@ -87,7 +87,7 @@ const Add = ({url}) => {
                 </div>
                 <div className="add-price flex-col">
                     <p>Product Price</p>
-                    <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='$20'  />
+                    <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='&#x20B9; 20'  />
                 </div>
             </div>
             <button type='submit' className='add-button'>ADD</button>
