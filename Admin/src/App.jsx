d15c2e3d4c2bff9cf016beb591fef728b30b6af7
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-01 07:40:49 +0530

    App

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,6 +9,7 @@ import WelcomePage from './pages/WelcomePage/WelcomePage'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import UpdateFoodDetails from './pages/UpdateFoodDetails/UpdateFoodDetails'
+import AddCategory from './pages/AddCategory/AddCategory'
 
 const App = () => {
 
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/list' element={<List url={url}/>} />
           <Route path='/orders' element={<Orders url={url}/>} />
           {/* <Route path='/update' element={<UpdateFoodDetails url={url}/>} /> */}
+          {/* <Route path='/UpdateCategory' element={<AddCategory url={url}/>}/> */}
         </Routes>
       </div>
     </>
