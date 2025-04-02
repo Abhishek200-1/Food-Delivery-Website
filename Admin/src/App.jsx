7d7c69b620acf467d74f3ef6d0c41c0e26a0d0cb
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-02 19:57:32 +0530

    added features

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,40 +1,64 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
-import WelcomePage from './pages/WelcomePage/WelcomePage'
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-import UpdateFoodDetails from './pages/UpdateFoodDetails/UpdateFoodDetails'
-import AddCategory from './pages/AddCategory/AddCategory'
-import AdminFeedback from './pages/AdminFeedback/AdminFeedback'
+import 'react-toastify/dist/ReactToastify.css';
+import AdminLogin from './pages/AdminLogin/AdminLogin';
+import AdminFeedback from './pages/AdminFeedback/AdminFeedback';
 
 const App = () => {
+  const url = "http://localhost:3000";
 
-  const url = "http://localhost:3000"
+  // Initialize state based on localStorage to prevent flicker on reload
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("adminToken"));
+  const [showLoginPopup, setShowLoginPopup] = useState(!localStorage.getItem("adminToken"));
+
+  useEffect(() => {
+    if (localStorage.getItem("adminToken")) {
+      setIsAuthenticated(true);
+      setShowLoginPopup(false);
+    }
+  }, []);
+
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+    setShowLoginPopup(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("adminToken");
+    setIsAuthenticated(false);
+    setShowLoginPopup(true);
+  };
 
   return (
     <>
-      <ToastContainer/>
-      <Navbar/>
-      <hr />
-      <div className="app-content">
-        <Sidebar/>
-        <Routes>
-          <Route path='/' element={<WelcomePage url={url}/>} />
-          <Route path='/add' element={<Add url={url}/>} />
-          <Route path='/list' element={<List url={url}/>} />
-          <Route path='/orders' element={<Orders url={url}/>} />
-          {/* <Route path='/update' element={<UpdateFoodDetails url={url}/>} /> */}
-          {/* <Route path='/UpdateCategory' element={<AddCategory url={url}/>}/> */}
-          <Route path='/AdminFeedback' element={<AdminFeedback url={url}/>}/>
-        </Routes>
-      </div>
+      <ToastContainer />
+      {showLoginPopup ? (
+        <AdminLogin onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <>
+          <Navbar onLogout={handleLogout} />
+          <hr />
+          <div className="app-content">
+            <Sidebar />
+            <Routes>
+              <Route path='/' element={<WelcomePage url={url} />} />
+              <Route path='/add' element={<Add url={url} />} />
+              <Route path='/list' element={<List url={url} />} />
+              <Route path='/orders' element={<Orders url={url} />} />
+              <Route path='/AdminFeedback' element={<AdminFeedback url={url} />} />
+            </Routes>
+          </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
