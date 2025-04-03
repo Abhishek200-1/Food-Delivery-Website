eb0e0358818ae9f1d974d214f794f23bae4c01bb
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-03 11:17:25 +0530

    updated

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,6 +1,5 @@
-// routes/adminRoute.js
 import express from 'express';
-import { loginAdmin, registerAdmin, changeAdminPassword } from '../controllers/adminController.js'; // Add registerAdmin function import
+import { loginAdmin, registerAdmin, changeAdminPassword, getAdminProfile, updateAdminProfile } from '../controllers/adminController.js';
 import authMiddleware from '../middleware/adminAuthMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +7,7 @@ const router = express.Router();
 router.post('/login', loginAdmin);
 router.post('/register', registerAdmin);
 router.post('/change-password', authMiddleware, changeAdminPassword);
+router.get('/profile', authMiddleware, getAdminProfile);
+router.put('/profile', authMiddleware, updateAdminProfile);
 
 export default router;
