ec9dd5db75bb093807bcb3746e86384a7d2df01b
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-03 11:17:17 +0530

    updated

--- Backend/models/adminModel.js
+++ Backend/models/adminModel.js
@@ -1,9 +1,14 @@
-// models/adminModel.js
 import mongoose from 'mongoose';
 
 const adminSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dateOfJoining: { type: Date, required: true },
   email: { type: String, required: true, unique: true },
+  mobileNumber: { type: String, required: true },
+  address: { type: String, required: true },
   password: { type: String, required: true },
+  profilePic: { type: String, default: "" }, 
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
