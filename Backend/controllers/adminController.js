afbbd61741f816ab74eb298a37492408c5808f20
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-05 22:40:01 +0530

    updated

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -47,6 +47,7 @@ export const getAdminProfile = async (req, res) => {
   }
 };
 
+//admin change password
 export const changeAdminPassword = async (req, res) => {
   const { currentPassword, newPassword } = req.body;
   const adminId = req.admin.id;
