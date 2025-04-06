1618bebb2b71142b9e561278755e4376c289b54f
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-04-06 11:23:10 +0530

    updated

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { loginUser, registerUser, getUserProfile } from '../controllers/userController.js';
+import authMiddlerware from '../middleware/auth.js'
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-
+userRouter.get("/profile", authMiddlerware, getUserProfile);
 
 
 export default userRouter;
